f94389dcd1b362945210267a3a5755521c747704
padrisimo <chamizho@gmail.com>
2020-03-09 17:00:31 +0100

    filter and delete todos

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
     <div class="container">
-<!--       <AddTodo/>
-      <FilterTodos/> -->
-      <Todos/>
+      <AddTodo />
+      <FilterTodos />
+      <Todos />
     </div>
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import FilterTodos from "./components/FilterTodos.vue";
 
 export default {
   name: "App",
   components: {
-    Todos
+    Todos,
+    AddTodo,
+    FilterTodos
   }
 };
 </script>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,17 +4,24 @@
     <div class="todos">
       <div v-for="todo in allTodos" :key="todo.id" class="todo">
         {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Todos",
-  computed: mapGetters(["allTodos"])
+  methods: {
+    ...mapActions(["fetchTodos", "deleteTodo"])
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  }
 };
 </script>
 
@@ -32,5 +39,38 @@ export default {
   text-align: center;
   position: relative;
   cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
